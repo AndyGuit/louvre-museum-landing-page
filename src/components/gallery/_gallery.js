67d65f0547db6ad36c7a21b5b89bef35a2216af4
AndyGuit <totalslash@msn.com>
2022-10-10 22:32:44 +0300

    feat: add gallery

---
+++ src/components/gallery/_gallery.js
@@ -0,0 +1,29 @@
+class Gallery {
+  constructor(picContainer, numOfImages) {
+    this.container = document.querySelector(picContainer);
+    this.imgArrSrc = Array.from({ length: numOfImages }, (_, i) => {
+      return `./img/gallery/gallery${i + 1}.jpg`
+    });
+
+    this.generateRandomImages();
+  }
+
+  generateRandomImages() {
+    this.imgArrSrc.sort(() => Math.random() - 0.5);
+
+    this.imgArrSrc.forEach(pic => {
+      const div = document.createElement('div');
+      div.classList.add('gallery__picture');
+
+      console.log(pic)
+
+      const img = document.createElement('img');
+      img.src = pic;
+      img.alt = pic;
+      this.container.append(div);
+      div.append(img);
+    })
+  }
+}
+
+export const gallery = new Gallery('.gallery__pictures', 15);
--- src/js/app.js
+++ src/js/app.js
@@ -5,5 +5,6 @@ import { datepicker } from '../components/datepicker/_datepicker.js'
 import { timepicker } from '../components/timepicker/_timepicker.js';
 import { dropdown } from '../components/dropdown/_dropdown.js';
 import { comparisonSlider } from '../components/comparison-slider/_comparison-slider.js';
+import { gallery } from '../components/gallery/_gallery.js';
 
 flsFunctions.isWebp();
