b52080e3c51a01e8b90bbb9c63d19600ae5f1da9
AndyGuit <totalslash@msn.com>
2022-10-25 23:13:08 +0300

    feat: add explore section

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,13 @@ import flatpickr from 'flatpickr';
 
 import { navMenu } from '../components/nav/_nav.js';
 import { sliderMain } from '../components/slider-main/_slider-main.js';
+import { comparisonSlider } from '../components/comparison-slider/_comparison-slider.js';
+
 
 
 // import { datepicker } from '../components/datepicker/_datepicker.js';
 // import { timepicker } from '../components/timepicker/_timepicker.js';
 // import { dropdown } from '../components/dropdown/_dropdown.js';
-// import { comparisonSlider } from '../components/comparison-slider/_comparison-slider.js';
 // import { gallery } from '../components/gallery/_gallery.js';
 // import map from '../components/map/_map.js';
 // import { sliderVideo } from '../components/slider-video/_slider-video.js';
