6348cc590b10226407506449aa6f00f6c71bafa6
AndyGuit <totalslash@msn.com>
2022-10-28 08:11:08 +0300

    feat: add gallery responsive design

--- src/components/gallery/_gallery.js
+++ src/components/gallery/_gallery.js
@@ -5,6 +5,7 @@ class Gallery {
       return `./img/gallery/gallery${i + 1}.jpg`
     });
 
+    this.generatePictureColumns();
     this.observeSection();
   }
 
@@ -35,24 +36,35 @@ class Gallery {
   }
 
   generateGalleryOnScroll = ([entry]) => {
+    this.container.innerHTML = '';
+    this.generatePictureColumns();
+
     const isVisible = entry.isIntersecting;
 
     isVisible ? this.generateRandomImages() : null;
   }
 
-  generateRandomImages = () => {
-    this.container.innerHTML = '';
+  generatePictureColumns = () => {
+    for (let i = 0; i < 3; i++) {
+      const div = document.createElement('div');
+      div.classList.add(`gallery__picture-col--${i}`);
+      this.container.append(div);
+    }
+  }
 
+  generateRandomImages = () => {
     this.imgArrSrc.sort(() => Math.random() - 0.5);
 
-    this.imgArrSrc.forEach(pic => {
+    this.imgArrSrc.forEach((pic, i) => {
       const div = document.createElement('div');
       div.classList.add('gallery__picture');
 
       const img = document.createElement('img');
       img.src = pic;
       img.alt = pic;
-      this.container.append(div);
+
+      const colEl = this.container.querySelector(`.gallery__picture-col--${i % 3}`);
+      colEl.append(div);
       div.append(img);
 
       this.observeElement(div);
