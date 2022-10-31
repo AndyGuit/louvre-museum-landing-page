844650f6677bb2e18c68de58ca985f4b5574d259
AndyGuit <totalslash@msn.com>
2022-10-31 04:38:18 +0300

    feat: add contacts section

--- src/js/app.js
+++ src/js/app.js
@@ -8,11 +8,11 @@ import { sliderVideo } from '../components/slider-video/_slider-video.js';
 import { videoPlayer } from '../components/video-player/_video-player.js';
 import { gallery } from '../components/gallery/_gallery.js';
 import { ticketsSectionCalculator } from './classes/tickets-section-calc.js';
+import map from '../components/map/_map.js';
 
 // import { datepicker } from '../components/datepicker/_datepicker.js';
 // import { timepicker } from '../components/timepicker/_timepicker.js';
 // import { dropdown } from '../components/dropdown/_dropdown.js';
-// import map from '../components/map/_map.js';
 // import { sliderVideo } from '../components/slider-video/_slider-video.js';
 // import { videoPlayer } from '../components/video-player/_video-player.js';
 // import { creditCard } from '../components/credit-card/_credit-card.js';
